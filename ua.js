4a01f4a6e7d112acf151b1615aa244ea18d3bee3
showzyl <showzyl520@gmail.com>
2016-08-26 22:05:14 +0800

    ua back up
    ua back up

---
+++ ua.js
@@ -0,0 +1,6 @@
+/**
+ * Created by showzyl on 8/26/16.
+ */
+
+
+// Mozilla/5.0 (Linux; U; Android 4.4.4; en-US; XT1022 Build/KXC21.5-40) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 UCBrowser/10.7.0.636 U3/0.8.0 Mobile Safari/534.30
