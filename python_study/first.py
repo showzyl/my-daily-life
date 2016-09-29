c200de4e13c600cc0cd73460f901561ee9f27fec
showzyl <showzyl520@gmail.com>
2016-09-29 15:13:00 +0800

    python
    python

---
+++ python_study/first.py
@@ -0,0 +1,99 @@
+# if statement
+
+# a = 100;
+# if a > 0:
+#   print a
+# else:
+#   print -a
+
+
+# print like typeing input
+# name = raw_input('what is ur name: ')
+# print 'hello', name
+
+
+# print "i'm fine"
+# print 'i\'m fine'
+# x = 10
+# x = x + 2
+# print x
+
+
+# a = 'ABC'
+# b = a
+# a = 'XYZ'
+# print b
+# print a
+
+
+# classmates = ['Michael', 'Bob', 'Tracy']
+# print classmates
+# print len(classmates)
+# print classmates[3]
+
+
+# test = (1, 2)
+# print test
+# print test[1]
+
+
+# age = 3
+# if age >= 18:
+#   print 'your age is', age
+#   print 'adult'
+# else:
+#   print 'tingger'
+
+# names = ['Michael', 'Bob', 'Tracy']
+# for name in names:
+#   print name
+
+# sum = 0
+# aTmp = [0,1,2,3]
+# for i in aTmp:
+#   sum += i
+#   #print i
+# print sum
+
+# aTmp = range(2, 10)
+# print aTmp
+
+# sum = 0
+# n = 99
+# while n > 0:
+#   sum = sum + n
+#   n = n - 2
+# print sum
+
+# d = {'Michael': 95, 'Bob': 75, 'Tracy': 85}
+# d['lizi'] = 12
+# print d
+# print d.get('Michael')
+# print abs(-200)
+# print cmp(1,2)
+
+
+# print int('123')
+# print float('1.2323')
+# print str(123)
+# print bool(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
