bf9ce70aa42631f40287c1f91300d24ba9918471
showzyl <showzyl520@gmail.com>
2016-10-10 21:35:25 +0800

    py
    py

--- python_study/first.py
+++ python_study/first.py
@@ -208,26 +208,77 @@
 # now()
 
 
-#!/usr/bin/env python
+# !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 ' a test module '
 
-__author__ = 'Michael Liao'
 
-import sys
+# __author__ = 'Michael Liao'
+#
+# import sys
+#
+# def test():
+#     args = sys.argv
+#     if len(args)==1:
+#         print 'Hello, world!'
+#     elif len(args)==2:
+#         print 'Hello, %s!' % args[1]
+#     else:
+#         print 'Too many arguments!'
+#
+# if __name__=='__main__':
+#     test()
+#
+# class Student(object):
+#   pass
+#
+# bart = Student()
+#
+# print bart
+# print Student
 
-def test():
-    args = sys.argv
-    if len(args)==1:
-        print 'Hello, world!'
-    elif len(args)==2:
-        print 'Hello, %s!' % args[1]
-    else:
-        print 'Too many arguments!'
 
-if __name__=='__main__':
-    test()
+# class Student(object):
+#   def __init__(self, name, score):
+#     self.name = name
+#     self.score = score
+#
+# def printScore(std):
+#   print '%s: %s' % (std.name, std.score)
+#
+# bart = Student('Bart Simpson', 59)
+#
+# printScore(bart)
+
+
+# bart = Student('Bart Simpson', 59)
+# print bart.name
+# print bart.score
+
+
+
+class Student(object):
+  def __init__(self, name, score):
+    self.name = name
+    self.score = score
+
+  def print_score(self):
+    print '%s: %s' % (self.name, self.score)
+
+bart = Student('Bart Simpson', 60)
+bart.print_score()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
