bf23a06cf9c2bf18520f6f1271c28257d002f3d6
showzyl <showzyl520@gmail.com>
2017-02-28 12:00:09 +0800

    stage
    stage

---
+++ front_end/stage.md
@@ -0,0 +1,42 @@
+### stage.js 分析
+
+```
+ lib
+    - anim.js
+    - atlas.js
+    - canvas.js
+    - core.js
+    - event.js
+    - image.js
+    - index.js
+    - layout.js
+    - license.js
+    - loop.js
+    - matrix.js
+    - pin.js
+    - root.js
+    - str.js
+    - texture.js
+    - tree.js
+    addon
+        - easing.js
+        - mouse.js
+        - tween.js
+    loader
+        - cordova.js
+        - web.js
+    util
+        - await.js
+        - create.js
+        - event.js
+        - extend.js
+        - insert.js
+        - is.js // 判断数据类型 copy from underscore
+        - math.js
+        - memo.js
+        - now.js
+        - once.js
+        - repeat.js
+        - stats.js
+        - string.js
+```
--- node_study/walk_file.js
+++ node_study/walk_file.js
@@ -13,44 +13,44 @@ let aFileList = [];
 
 
 walk('/home/zengyuanli/caipiao/onebuy/src/');
-aFileList.map( item => console.log(item) );
+aFileList.map(item => console.log(item));
 
 
-function walk(path){
-  let aDirList = fs.readdirSync(path);
-  // 记录当天修改的文件
-  aDirList.map( item => {
-    if(!/^\./.test(item)){
-    var oItem = fs.statSync(path + '/' + item);
-    if( oItem.isFile() && oItem.mtime>oTime.start && oItem.mtime<oTime.end ){
-      aFileList.push( path + '/' + item );
-    }
-  }
-});
-  // 递归遍历目录
-  aDirList.map( item => {
-    if(fs.statSync(path + '/' + item).isDirectory()){
-    if(!/^\./.test(item)){
-      walk(path + '/' + item);
-    }
-  }
-});
+function walk(path) {
+	let aDirList = fs.readdirSync(path);
+	// 记录当天修改的文件
+	aDirList.map(item => {
+		if (!/^\./.test(item)) {
+			var oItem = fs.statSync(path + '/' + item);
+			if (oItem.isFile() && oItem.mtime > oTime.start && oItem.mtime < oTime.end) {
+				aFileList.push(path + '/' + item);
+			}
+		}
+	});
+	// 递归遍历目录
+	aDirList.map(item => {
+		if (fs.statSync(path + '/' + item).isDirectory()) {
+			if (!/^\./.test(item)) {
+				walk(path + '/' + item);
+			}
+		}
+	});
 
 }
 
-function calculateTime(){
-  let todayStart = new Date();
-  let todayEnd = new Date();
-  todayStart.setHours(0);
-  todayStart.setMinutes(0);
-  todayStart.setSeconds(0);
-  todayEnd.setHours(23);
-  todayEnd.setMinutes(59);
-  todayEnd.setSeconds(59);
-  return {
-    start: todayStart,
-    end: todayEnd
-  }
+function calculateTime() {
+	let todayStart = new Date();
+	let todayEnd = new Date();
+	todayStart.setHours(0);
+	todayStart.setMinutes(0);
+	todayStart.setSeconds(0);
+	todayEnd.setHours(23);
+	todayEnd.setMinutes(59);
+	todayEnd.setSeconds(59);
+	return {
+		start: todayStart,
+		end: todayEnd
+	}
 }
 
 
