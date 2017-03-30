15659e29e7516f68fa4089b61c0b1ad807dc9bc7
showzyl <showzyl520@gmail.com>
2017-03-30 10:54:42 +0800

    add rever value
    add rever value

--- node_study/revert_value.js
+++ node_study/revert_value.js
@@ -1,13 +1,35 @@
 /**
- * Created by showzyl on 2017/3/25.
+ *  转换文件每一行的`key`和`value`
  */
 
-'use strict'
+'use strict';
 
-/**
- * 开个坑
- */
+const fs = require('fs');
+const readline = require('readline');
 
+let lineReader = readline.createInterface({
+	input: fs.createReadStream('./ttt.txt'),
+	output: ''
+});
+
+let aRes = [];
+let a0 = [];
+let a1 = [];
+let a2 = [];
+
+lineReader.on('line', function (line) {
+	let aTmp = line.split(/\s/);
+	a0.push(aTmp[0]);
+	a1.push(aTmp[1]);
+	a2.push(aTmp[2]);
+});
+
+lineReader.on('close', function () {
+	for(let i = 0; i < a0.length; i++){
+		aRes.push(a0[i] + a1[i] + a2[a0.length-1-i]);
+	}
+	console.log(aRes);
+});
 
 
 
