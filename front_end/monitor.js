0ed0a713ae9d2617db4b0c6c4a892f1e31c6a05d
showzyl <showzyl520@gmail.com>
2017-04-17 02:45:28 +0800

    monitor

---
+++ front_end/monitor.js
@@ -0,0 +1,34 @@
+/**
+ * Created by showzyl on 2017/4/12.
+ */
+
+'use strict';
+
+/**
+ *  `前端异常监控`
+ */
+
+;(function (win) {
+	console.log(win);
+	/**
+	 *  捕捉报错
+	 */
+
+
+	/**
+	 *  异常上报
+	 */
+
+
+
+})(window);
+
+
+
+
+
+
+
+
+
+
