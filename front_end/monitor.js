7ec3101bd3365f90b45c7b1b7b81cb8ee8bf7b4e
showzyl <showzyl520@gmail.com>
2017-05-01 02:52:28 +0800

    monitor

--- front_end/monitor.js
+++ front_end/monitor.js
@@ -6,25 +6,63 @@
 
 /**
  *  `前端异常监控`
+ *  
+ *  1. when use `window.onerror` err request can't be catched
+ *  2. when get a err have to report to `GA` at the same time
+ *  3. 
+ *  
+ *  
  */
 
 ;(function (win) {
-	console.log(win);
-	/**
-	 *  捕捉报错
-	 */
-	function getErr() {
-		window.onerror = function () {
 
+	function Monitor() {
+		this.isInit = false;
+		
+	}
+
+	Monitor.prototype = {
+		/**
+		 *  捕捉报错
+		 */
+		getErr: function () {
+			window.onerror = function (msg, url, line, col, err) {
+				var o = {
+					'msg: ': msg,
+					'url: ': url,
+					'line: ': line,
+					'col: ': col,
+					'err: ': err.stack
+				};
+
+				this.sendErr(o);
+				return true;
+			}
+		},
+
+		/**
+		 *  异常上报
+		 */
+		sendErr: function (obj) {
+			console.log(JSON.stringify(obj));
+			
 		}
-	}
+		
+	};
 
-	/**
-	 *  异常上报
-	 */
-	function sendErr() {
+	var m = new Monitor();
+	console.log(m);
+	m.getErr();
 
-	}
+	setTimeout(function(){
+		wqddwq9();
+	},1000);
+
+	setTimeout(function(){
+		throw Error('握草');
+	},1200);
+
+	aaa();
 
 
 
