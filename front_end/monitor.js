24e2719492bc7acd6eaef7ea10538c76e6c58946
showzyl <showzyl520@gmail.com>
2017-05-06 00:16:12 +0800

    modify monitor

--- front_end/monitor.js
+++ front_end/monitor.js
@@ -1,69 +1,123 @@
 /**
- * Created by showzyl on 2017/4/12.
+ * Created by showzyl on 2017/5/5.
  */
 
 'use strict';
 
 /**
  *  `前端异常监控`
- *  
+ *
  *  1. when use `window.onerror` err request can't be catched
- *  2. when get a err have to report to `GA` at the same time
- *  3. 
- *  
- *  
+ *  2. when get an err have to async report to `GA`
+ *  3.
+ *
+ *
  */
 
-;(function (win) {
 
-	function Monitor() {
-		this.isInit = false;
-		
-	}
+;(function (w) {
 
-	Monitor.prototype = {
-		/**
-		 *  捕捉报错
-		 */
-		getErr: function () {
-			window.onerror = function (msg, url, line, col, err) {
-				var o = {
-					'msg: ': msg,
-					'url: ': url,
-					'line: ': line,
-					'col: ': col,
-					'err: ': err.stack
-				};
+	w.Monitor = w.Monitor || {};
 
-				this.sendErr(o);
-				return true;
-			}
-		},
+	var Monitor = w.Monitor;
 
-		/**
-		 *  异常上报
-		 */
-		sendErr: function (obj) {
-			console.log(JSON.stringify(obj));
-			
+	/**
+	 *
+	 * @param cb
+	 * @param ua
+	 */
+	Monitor.stats = function (cb, ua, time) {
+		if(typeof ga === 'undefined'){
+			(function (i, s, o, g, r, a, m) {
+				i['GoogleAnalyticsObject'] = r;
+				i[r] = i[r] || function () {
+						(i[r].q = i[r].q || []).push(arguments)
+					}, i[r].l = 1 * new Date();
+				a = s.createElement(o),
+					m = s.getElementsByTagName(o)[0];
+				a.async = 1;
+				a.src = g;
+				m.parentNode.insertBefore(a, m)
+			})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+			ga('create', ua || 'wqdwqdwq', 'auto');
+			ga('set', 'checkProtocolTask', null); // Disable file protocol checking.
+			ga('set', 'checkStorageTask', null); // Disable cookie storage checking.
+			ga('set', 'historyImportTask', null); // Disable history checking (requires reading from cookies).
+			ga('send', 'pageview'); // 统计`pv`
 		}
-		
+		// 异步上报
+		if(cb){
+			setTimeout(function () {
+				cb();
+			}, time || 300);
+		}
 	};
 
-	var m = new Monitor();
-	console.log(m);
-	m.getErr();
+	/**
+	 *
+	 * @param type `globe lost catch warning`
+	 * @param obj
+	 */
+	Monitor.notify = function (type, msg) {
+		console.log(type, msg);
 
-	setTimeout(function(){
-		wqddwq9();
-	},1000);
+		var dataType = {}.toString.call(msg);
 
-	setTimeout(function(){
-		throw Error('握草');
-	},1200);
+		if (dataType === '[object Object]') {
+			msg['type'] = type;
+		} else if (dataType === '[object String]') {
 
-	aaa();
+		}
 
+		Monitor.stats(function () {
+
+		})
+
+	};
+
+
+	/**
+	 * catch globle error
+	 * @param msg
+	 * @param url
+	 * @param line
+	 * @param col
+	 * @param err
+	 * @returns {boolean}
+	 */
+	w.onerror = function (msg, url, line, col, err) {
+		var errStack = '';
+		if (err && err.stack) {
+			errStack = _handleErrMsg(err.stack);
+		}
+		var o = {
+			msg: msg || '',
+			url: url || '',
+			line: line || 0,
+			col: col || 0,
+			err: errStack
+		};
+		var msg = JSON.stringify(o);
+		Monitor.notify('globe', msg);
+		return false; // still log in console
+	};
+
+	/**
+	 *
+	 * @param err
+	 * @returns {string}
+	 * @private
+	 */
+	function _handleErrMsg(err) {
+		return err.replace(/\n/gi, '')            // 去掉换行
+			.replace(/\bat\b/gi, '@')       // chrome中是at，ff中是@
+			.split('@')                     // 以@分割信息
+			.slice(0, 10)                    // 最大堆栈长度 Error.stackTraceLimit 10
+			//.map(v => v.replace(/^\s*|\s*$/g, ''))  //取出多余空格
+			.map(function (v) { return v.replace(/^\s*|\s*$/g, ''); })  //取出多余空格
+			.join('~')                      // 手动添加分隔符 便于后期展示
+			.replace(/\?[^:]+/gi, '');      // 去除js文件多余参数(?x=1之类)
+	}
 
 
 })(window);
