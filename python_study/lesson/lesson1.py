4bd06403cf37f36ca98e4921371ebaac2f19a952
showzyl <showzyl520@gmail.com>
2017-09-09 00:24:33 +0800

    add lesson1

---
+++ python_study/lesson/lesson1.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by showzyl on 2017/9/7
+
+
+# # 多行语句
+# total = 'lizi ' + \
+#         'test'
+#
+# print total
+
+'''
+多行注释
+test
+'''
+
+# #段落
+# paragraph = """这是一个段落。
+# 包含了多个语句"""
+#
+# print paragraph
+
+import sys
+# x = 'foo'
+# sys.stdout.write(x + '\n')
+# sys.stdout.write('test')
+
+# # 删除变量
+# var = 123
+# del var
+# print var
+
+# # 字符串切片
+# s = 'Hello World!'
+# print s[2:5]
+
+# # 数组切片
+# a = [1, 'b', 'c']
+# print a[1:2]
+# print a[1:]
+
+
+# 某数是否在数组里
+a = '1'
+b = 20
+list = [5, 2, 3, 4, '1']
+
+if ( a in list ):
+    print "Line 1 - a is available in the given list"
+else:
+    print "not in"
+
+
+
+if __name__ == '__main__':
+    pass
+
