71c1e41bced1233f8a821fde429678e59cbe8d10
showzyl <showzyl520@gmail.com>
2017-09-14 00:41:26 +0800

    add lesson4

---
+++ python_study/lesson/lesson4.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by showzyl on 2017/9/11
+
+
+from __future__ import division
+from numpy.random import randn
+import numpy as np
+
+arr = np.arange(10)
+
+print arr
+
+
+
+# `曼哈顿距离` python 实现
+from numpy import *
+vector1 = mat([1,2,3])
+vector2 = mat([4,5,6])
+print sum(abs(vector1-vector2))
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
+
