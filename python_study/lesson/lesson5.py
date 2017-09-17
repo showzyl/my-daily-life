2af7545f699254817308bc53578bda4ad5cd121c
showzyl <showzyl520@gmail.com>
2017-09-18 00:09:44 +0800

    add lesson5

---
+++ python_study/lesson/lesson5.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by showzyl on 2017/9/11
+
+import numpy as np
+import pandas as pd
+import sys
+from pandas import Series, DataFrame
+
+
+
+# obj = Series([4, 7, -5, 3])
+# print obj
+# print obj.values
+# print obj.index
+
+# obj2 = Series([4, 7, -5, 3], index=['d', 'b', 'a', 'c'])
+# print obj2
+# print obj2.index
+# print obj2['a']
+# obj2['d'] = 6
+# print obj2
+# print obj2[['c', 'a', 'd']]
+# print obj2[obj2 > 0]
+# print obj2 * 2
+
+
+# sdata = {'Ohio': 35000, 'Texas': 71000, 'Oregon': 16000, 'Utah': 5000}
+# obj3 = Series(sdata)
+# print obj3
+
+# states = ['California', 'Ohio', 'Oregon', 'Texas']
+# obj4 = Series(sdata, index=states)
+# print obj4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
+
