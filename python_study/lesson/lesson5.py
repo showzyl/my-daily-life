2773f4e3586f2b7d592ccda37781514a3b994a30
showzyl <showzyl520@gmail.com>
2017-09-19 20:51:36 +0800

    add lesson5

--- python_study/lesson/lesson5.py
+++ python_study/lesson/lesson5.py
@@ -23,7 +23,7 @@ from pandas import Series, DataFrame
 # print obj2[['c', 'a', 'd']]
 # print obj2[obj2 > 0]
 # print obj2 * 2
-
+# print 'b' in obj2
 
 # sdata = {'Ohio': 35000, 'Texas': 71000, 'Oregon': 16000, 'Utah': 5000}
 # obj3 = Series(sdata)
@@ -32,6 +32,54 @@ from pandas import Series, DataFrame
 # states = ['California', 'Ohio', 'Oregon', 'Texas']
 # obj4 = Series(sdata, index=states)
 # print obj4
+# print pd.isnull(obj4)
+# print '------------------------'
+# print pd.notnull(obj4)
+# print '------------------------'
+# print obj4.isnull()
+
+# print obj3
+# print '------------------------'
+# print obj4
+# print '------------------------'
+# print obj3 + obj4
+
+# obj4.name = 'population'
+# obj4.index.name = 'state'
+# print obj4
+
+# dataframe
+# data = {'state': ['Ohio', 'Ohio', 'Ohio', 'Nevada', 'Nevada'],
+#         'year': [2000, 2001, 2002, 2001, 2002],
+#         'pop': [1.5, 1.7, 3.6, 2.4, 2.9]}
+# frame = DataFrame(data)
+# print frame
+# print DataFrame(data, columns=['year', 'state', 'pop'])
+
+# frame2 = DataFrame(data, columns=['year', 'state', 'pop', 'debt'],
+#                    index=['one', 'two', 'three', 'four', 'five'])
+
+# print frame2
+# print '------------------------'
+# print frame2.columns
+
+# print frame2['state']
+# print frame2.year
+# print frame2.ix['three']
+
+pop = {'Nevada': {2001: 2.4, 2002: 2.9},
+       'Ohio': {2000: 1.5, 2001: 1.7, 2002: 3.6}}
+frame3 = DataFrame(pop)
+print frame3
+print frame3.T
+
+
+
+
+
+
+
+
 
 
 
