88395c482d07a4521b86f4142bbb3e79ed043c8f
showzyl <showzyl520@gmail.com>
2017-10-28 00:43:40 +0800

    add const

--- go/hello.go
+++ go/hello.go
@@ -3,9 +3,7 @@ package main
 import "fmt"
 
 func main(){
-	var test = "lizi ";
-	name := test; // 左边定义的变量必须是没被定义过的
-	// var age int = 10;
+	name := "hello"; // 左边定义的变量必须是没被定义过的
 	fmt.Println(name)
 }
 
