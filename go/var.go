db31ae437b77b2d39f43f93a22f82582bfd5ece9
showzyl <showzyl520@gmail.com>
2017-10-27 22:55:31 +0800

    add var

---
+++ go/var.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+	var a string = "test"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b + c)
+
+	var d = false
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+}
+
